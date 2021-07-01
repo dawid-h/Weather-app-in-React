a0b224925cf8ae80dada5752690b329892df96ac
Dawid Herman <d.herman@student.uw.edu.pl>
2021-07-01 18:40:17 +0200

    fixed version of different forecast periods

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -2,60 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useWeatherAPI } from "../hooks/useWeatherAPI";
 
-/* The example of an API answer:
-{
-  "location": {
-    "name": "Mosty",
-    "region": "",
-    "country": "Poland",
-    "lat": 54.62,
-    "lon": 18.47,
-    "tz_id": "Europe/Warsaw",
-    "localtime_epoch": 1625041964,
-    "localtime": "2021-06-30 10:32"
-  },
-  "current": {
-    "last_updated_epoch": 1625040900,
-    "last_updated": "2021-06-30 10:15",
-    "temp_c": 26,
-    "temp_f": 78.8,
-    "is_day": 1,
-    "condition": {
-      "text": "Sunny",
-      "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
-      "code": 1000
-    },
-    "wind_mph": 11.9,
-    "wind_kph": 19.1,
-    "wind_degree": 120,
-    "wind_dir": "ESE",
-    "pressure_mb": 1008,
-    "pressure_in": 30.2,
-    "precip_mm": 0,
-    "precip_in": 0,
-    "humidity": 39,
-    "cloud": 0,
-    "feelslike_c": 27.5,
-    "feelslike_f": 81.5,
-    "vis_km": 10,
-    "vis_miles": 6,
-    "uv": 6,
-    "gust_mph": 8.9,
-    "gust_kph": 14.4
-  }
-}
-*/
-
 export function Forecast() {
   const period = useSelector(x => x.period);
   const place = useSelector(x => x.place);
-  useEffect(() => {
-    console.log("hejka");
-    setState({
-      loading: true,
-      forecast: {}
-    });
-  }, [period, place]);
   const [state, setState] = useState({
     loading: true,
     forecast: {}
@@ -77,6 +26,14 @@ export function Forecast() {
         setState({loading: false, forecast: {}});
       });
   }
+
+  useEffect(() => {
+    console.log("hejka");
+    setState({
+      loading: true,
+      forecast: {}
+    });
+  }, [period, place]);
 
   if (state.loading) {
     if (Object.keys(place).length === 0)
@@ -101,10 +58,12 @@ export function Forecast() {
       console.log(state.forecast);
       switch (period) {
         case 'DAILY':
-          return (
+          if (state.forecast.forecast === undefined)
+            setState({loading: true, forecast: {}});
+          else return (
             <div>
               <h1>{state.forecast.location.name}, {state.forecast.location.country}</h1>
-              <div>
+              <span>
                 <h2>Dzisiaj</h2>
                 <h3>
                   {state.forecast.forecast.forecastday[0].day.maxtemp_c}°C
@@ -114,8 +73,8 @@ export function Forecast() {
                 </h3>
                 <img src={`http:${state.forecast.forecast.forecastday[0].day.condition.icon}`} alt="" />
                 <p>{state.forecast.forecast.forecastday[0].day.condition.text}</p>
-              </div>
-              <div>
+              </span>
+              <span>
                 <h2>Jutro</h2>
                 <h3>
                   {state.forecast.forecast.forecastday[1].day.maxtemp_c}°C
@@ -125,8 +84,8 @@ export function Forecast() {
                 </h3>
                 <img src={`http:${state.forecast.forecast.forecastday[1].day.condition.icon}`} alt="" />
                 <p>{state.forecast.forecast.forecastday[1].day.condition.text}</p>
-              </div>
-              <div>
+              </span>
+              <span>
                 <h2>Pojutrze</h2>
                 <h3>
                   {state.forecast.forecast.forecastday[2].day.maxtemp_c}°C
@@ -136,13 +95,27 @@ export function Forecast() {
                 </h3>
                 <img src={`http:${state.forecast.forecast.forecastday[2].day.condition.icon}`} alt="" />
                 <p>{state.forecast.forecast.forecastday[2].day.condition.text}</p>
-              </div>
+              </span>
             </div>
           );
+          break;
         case 'HOURLY':
-          return (
-            <div>O tej porze nie wiem</div>
+          if (state.forecast.forecast === undefined)
+            setState({loading: true, forecast: {}});
+          else return (
+            <div>
+              <h1>{state.forecast.location.name}, {state.forecast.location.country}</h1>
+              {state.forecast.forecast.forecastday[0].hour.map(x =>
+                <div>
+                  <h2>{x.time.substr(x.time.length - 5)}</h2>
+                  <h3>{x.temp_c}°C</h3>
+                  <img src={`http:${x.condition.icon}`} alt="" />
+                  <p>{x.condition.text}</p>
+                </div>
+              )}
+            </div>
           );
+          break;
         case 'REALTIME':
         default:
           return (
