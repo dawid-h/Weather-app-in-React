f9d0ba8d27a01947caa7cbd08f03c88c0b0dbf71
Dawid Herman <d.herman@student.uw.edu.pl>
2021-09-15 22:04:59 +0200

    better state management in realtime component

--- src/components/forecast/components/realtime.js
+++ src/components/forecast/components/realtime.js
@@ -1,38 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { realtime$ } from "../../../hooks/useWeatherAPIRealtime";
 import SideInfo from "../../styles/SideInfo";
 import { WeatherGIF } from "./WeatherGIF";
 import { WeatherOverview } from "./WeatherOverview";
 
-export function Realtime(forecast) {
-  const [error, setError] = useState(false);
-  
-  realtime$(forecast.location.lat, forecast.location.lon).subscribe({
+export function Realtime(forecastP) {
+  const [state, setState] = useState({error: false, forecast: forecastP});
+  const subscription = realtime$(forecastP.location.lat, forecastP.location.lon).subscribe({
     next: data => {
-      forecast = {...forecast, ...data};
-      setError(false);
+      setState({error: false, forecast: {...state.forecast, ...data}});
     },
     error: err => {
-      setError(true);
+      setState({error: true, forecast: undefined});
       console.log(err);
     }
   });
 
+  useEffect(() => {
+    return () => subscription.unsubscribe();
+  }, [subscription]);
+
   function writeErrorIfOccurred() {
-    return error ? 'Loading current data failed' : '';
+    return state.error ? 'Loading current data failed' : '';
   }
 
   return (
     <div>
       <p>{writeErrorIfOccurred()}</p>
-      <h1>{forecast.location.name}, {forecast.location.country}</h1>
+      <h1>{state.forecast.location.name}, {state.forecast.location.country}</h1>
       <SideInfo>
-        <WeatherOverview {...forecast.forecast.forecastday} />
-        <WeatherGIF condition={forecast.current.condition.text} />
+        <WeatherOverview {...state.forecast.forecast.forecastday} />
+        <WeatherGIF condition={state.forecast.current.condition.text} />
       </SideInfo>
-      <h2>{forecast.current.temp_c}°C</h2>
-      <img src={`http:${forecast.current.condition.icon}`} alt="wether condition" />
-      <p>{forecast.current.condition.text}</p>
+      <h2>{state.forecast.current.temp_c}°C</h2>
+      <img src={`http:${state.forecast.current.condition.icon}`} alt="wether condition" />
+      <p>{state.forecast.current.condition.text}</p>
     </div>
   );
 }
